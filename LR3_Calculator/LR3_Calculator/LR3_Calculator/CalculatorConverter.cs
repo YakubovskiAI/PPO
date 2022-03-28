316217077fe3bbbea19c272b781e3e282893f9d5
YakubovskiAI <vlad.bovskiy@mail.ru>
2022-03-28 03:00:39 +0300

    Add LR3

---
+++ LR3_Calculator/LR3_Calculator/LR3_Calculator/CalculatorConverter.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class CalculatorConverter
+{
+    public static double CalculatePolandList(List<object> polandList)
+    {
+        var stack = new Stack<Double>();
+        double num1, num2;
+        foreach(object obj in polandList)
+        {
+            if (obj is Double)
+                stack.Push((Double)obj);
+            else
+            {
+                num2 = stack.Pop();
+                num1 = stack.Pop();
+                switch((Char)obj)
+                {
+                    case '+':
+                        stack.Push(num1 + num2);
+                        break;
+                    case '-':
+                        stack.Push(num1 - num2);
+                        break;
+                    case '*':
+                        stack.Push(num1 * num2);
+                        break;
+                    case '/':
+                        stack.Push(num1 / num2);
+                        break;
+                }
+            }
+        }
+        return stack.Pop();
+    }
+    public static List<object> ListToPolandList(List<object> list)
+    {
+        var operators = new List<char> { '+', '-', '*', '/' };
+        var operatorsPriority = new Dictionary<char, int>()
+        {
+            {'(', 0},
+            {'+', 1},
+            {'-', 1},
+            {'*', 2},
+            {'/', 2}
+        };
+        var polandList = new List<object>();
+        var texas = new Stack<Char>();
+
+        foreach (object obj in list)
+        {
+            if (obj is Double)
+            {
+                polandList.Add(obj);
+                continue;
+            }
+            if((Char)obj == '(')
+            {
+                texas.Push((Char)obj);
+                continue;
+            }
+            if ((Char)obj == ')')
+            {
+                while (texas.Peek() != '(')
+                    polandList.Add(texas.Pop());
+                texas.Pop();
+                continue;
+            }
+            if (operators.Contains((Char)obj))
+            {
+                while(texas.Count > 0 && operatorsPriority[texas.Peek()] > operatorsPriority[(Char)obj])
+                    polandList.Add(texas.Pop());
+                texas.Push((Char)obj);
+                continue;
+            }
+        }
+        while (texas.Count > 0)
+            polandList.Add(texas.Pop());
+        return polandList;
+    }
+    public static List<object> StringToList(string str)
+    {
+        if (str == null)
+            return null;
+
+        var list = new List<object>();
+        var readyChar = new List<char> { '+', '*', '/', '(', ')' };
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (readyChar.Contains(str[i]))
+            {
+                list.Add(str[i]);
+                continue;
+            }
+
+            if (Char.IsDigit(str[i]))
+            {
+                list.Add(Double.Parse(ReadStrNum(str, ref i)));
+                continue;
+            }
+
+
+            if (str[i] == '-')
+            {
+                if (i == 0)
+                {
+                    string d = ReadStrNum(str, ref i);
+                    list.Add(Double.Parse(d));
+                    continue;
+                }
+                if (str[i - 1] == '(')
+                {
+                    string d = ReadStrNum(str, ref i);
+                    list.Add(Double.Parse(d));
+                    continue;
+                }
+                list.Add(str[i]);
+            }
+        }
+        return list;
+    }
+    private static string ReadStrNum(string str, ref int i)
+    {
+        string number = "";
+        for (; i < str.Length; i++)
+        {
+            if (!Char.IsDigit(str[i]) && str[i] != ',')
+            {
+                if (number == "" && str[i] == '-')
+                {
+                    number += "-";
+                    continue;
+                }
+                break;
+            }
+            if (str[i] == ',')
+            {
+                number += '.';
+                continue;
+            }
+            number += str[i];
+        }
+        i--;
+        return number;
+    }
+}
---
+++ LR3_Calculator/LR3_Calculator/LR3_Calculator/MainPage.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LR3_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            Button_0.Clicked += Button_0_Clicked;
+            Button_1.Clicked += Button_1_Clicked;
+            Button_2.Clicked += Button_2_Clicked;
+            Button_3.Clicked += Button_3_Clicked;
+            Button_4.Clicked += Button_4_Clicked;
+            Button_5.Clicked += Button_5_Clicked;
+            Button_6.Clicked += Button_6_Clicked;
+            Button_7.Clicked += Button_7_Clicked;
+            Button_8.Clicked += Button_8_Clicked;
+            Button_9.Clicked += Button_9_Clicked;
+            Button_clear.Clicked += Button_clear_Clicked;
+            Button_open.Clicked += Button_open_Clicked;
+            Button_close.Clicked += Button_close_Clicked;
+            Button_erase.Clicked += Button_erase_Clicked;
+            Button_div.Clicked += Button_div_Clicked;
+            Button_mul.Clicked += Button_mul_Clicked;
+            Button_sub.Clicked += Button_sub_Clicked;
+            Button_add.Clicked += Button_add_Clicked;
+            Button_com.Clicked += Button_com_Clicked;
+            Button_eq.Clicked += Button_eq_Clicked;
+
+
+        }
+
+        private void Button_eq_Clicked(object sender, EventArgs e)
+        {
+            List<object> list = CalculatorConverter.StringToList(Label_bot.Text);
+            List<object> polandList = CalculatorConverter.ListToPolandList(list);
+            double result = CalculatorConverter.CalculatePolandList(polandList);
+
+            Label_top.Text = result.ToString();
+        }
+
+        private void Button_com_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += ",";
+        }
+
+        private void Button_add_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "+";
+        }
+
+        private void Button_sub_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "-";
+        }
+
+        private void Button_mul_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "*";
+        }
+
+        private void Button_div_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "/";
+        }
+
+        private void Button_erase_Clicked(object sender, EventArgs e)
+        {
+            string str = Label_bot.Text;
+            if (str == null || str == "")
+                return;
+            str = str.Substring(0, str.Length - 1);
+            Label_bot.Text = str;
+        }
+
+        private void Button_open_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "(";
+        }
+
+        private void Button_close_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += ")";
+        }
+
+        private void Button_clear_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text = "";
+            Label_top.Text = "";
+        }
+
+        private void Button_0_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "0";
+        }
+
+        private void Button_1_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "1";
+        }
+
+        private void Button_2_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "2";
+        }
+
+        private void Button_3_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "3";
+        }
+
+        private void Button_4_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "4";
+        }
+
+        private void Button_5_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "5";
+        }
+
+        private void Button_6_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "6";
+        }
+
+        private void Button_7_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "7";
+        }
+
+        private void Button_8_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "8";
+        }
+
+        private void Button_9_Clicked(object sender, EventArgs e)
+        {
+            Label_bot.Text += "9";
+        }
+    }
+}
